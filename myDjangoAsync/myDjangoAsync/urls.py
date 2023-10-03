d99dcc1d412fb773240b15acf755cd06edeeca44
Oier Mentxaka <oiermentxaka@gmail.com>
2023-10-03 11:31:08 +0200

    async seems to be working in simple functions

--- myDjangoAsync/myDjangoAsync/urls.py
+++ myDjangoAsync/myDjangoAsync/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myDjangoAsync import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('test1/', views.test1),
 ]
+
