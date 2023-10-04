c62c8d9b946d76e6e7fd1cb4cfdaecc4cc549611
Danel Arias <71316377+DanelArias-Dreyton257@users.noreply.github.com>
2023-10-04 10:13:09 +0200

    URLS

--- DjangoAsync/myDjangoAsync/myDjangoAsync/urls.py
+++ DjangoAsync/myDjangoAsync/myDjangoAsync/urls.py
@@ -13,3 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
+from myDjangoAsync import views
+
+urlpatterns = [
+    path('', views.index),
+    path('admin/', admin.site.urls),
+    path('test1/', views.test1),
+    path('check/', views.check),
+]
