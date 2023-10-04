9acbc1a46799af7329ac1fd7df3b104011110c84
Danel Arias <71316377+DanelArias-Dreyton257@users.noreply.github.com>
2023-10-04 09:59:54 +0200

    celery.py

--- DjangoAsync/myDjangoAsync/myDjangoAsync/views.py
+++ DjangoAsync/myDjangoAsync/myDjangoAsync/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import time
 from datetime import datetime
 import os
+from celery import shared_task
 
 
 def index(request):
@@ -16,12 +17,13 @@ def check(request):
         return HttpResponse("using ASGI")
 
 
-def test1(request):
+@shared_task
+async def test1(request):
     start_time = time.time()
     start_time_readable = datetime.fromtimestamp(
         start_time).strftime('%Y-%m-%d %H:%M:%S')
     print("Starting time: " + str(start_time_readable))
-    time.sleep(10)
+    await time.sleep(10)
     # i = 0
     # while time.time() < start_time + 10:
     #     print(i)
@@ -31,4 +33,6 @@ def test1(request):
         end_time).strftime('%Y-%m-%d %H:%M:%S')
     print("Ending time: " + str(end_time_readable))
     duration_in_seconds = end_time - start_time
-    return HttpResponse("Hello world! \nDuration: "+str(duration_in_seconds)+" \nStarting time: " + str(start_time_readable) + "\nEnding time: " + str(end_time_readable))
+    result = "Hello world! \nDuration: "+str(duration_in_seconds)+" \nStarting time: " + str(
+        start_time_readable) + "\nEnding time: " + str(end_time_readable)
+    return HttpResponse(result)
